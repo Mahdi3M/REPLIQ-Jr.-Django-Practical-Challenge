110137c808a38554e52cf93a3993cc2b7c60919a
Mahdi3M <mahdimohtasim@gmail.com>
2024-03-17 12:38:41 +0600

    Created new app "Tracker_App" -urls.py files configured. -app added to settings

---
+++ Corporate_Gadget_Tracker/Tracker_App/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from Tracker_App import views
+
+app_name = "Tracker_App"
+
+urlpatterns = [
+    # path('', views.signin, name="signin"),
+    # path('signout', views.sign_out, name="signout"),
+]
